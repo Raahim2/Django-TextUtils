6ca491a2d6471985f397226e4014430ace91a84b
Raahim <127599591+Raahim2@users.noreply.github.com>
2024-01-27 21:02:04 +0530

    Update settings.py

--- TextUtils/settings.py
+++ TextUtils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k4wf9yek@rop7t@qb9fgg(#2im@#bb_auq1qefwu$huu3%udk0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , 'now.sh']
 
 
 # Application definition
